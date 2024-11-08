25a879992897a240310fbb610b9b2d53a12330a1
michelleWYLee <michelle.lee.wing.yu@gmail.com>
2024-11-08 03:06:14 +0000

    Exercise04Task01

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -8,8 +8,18 @@ using Microsoft.Data.SqlClient;
 using Azure.AI.OpenAI;
 using Azure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build();
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,6 +39,19 @@ builder.Services.AddSingleton<CosmosClient>((_) =>
     );
     return client;
 });
+
+builder.Services.AddSingleton<Kernel>((_) =>
+{
+    IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+    kernelBuilder.AddAzureOpenAIChatCompletion(
+        deploymentName: builder.Configuration["AzureOpenAI:DeploymentName"]!,
+        endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+        apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+    );
+    kernelBuilder.Plugins.AddFromType<DatabaseService>();
+    return kernelBuilder.Build();
+});
+
 
 // Create a single instance of the AzureOpenAIClient to be shared across the application.
 builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
@@ -60,39 +83,49 @@ app.MapGet("/", async () =>
     .WithName("Index")
     .WithOpenApi();
 
-// Retrieve the set of hotels from the database.
-app.MapGet("/Hotels", async () => 
-{
-    throw new NotImplementedException();
-})
-    .WithName("GetHotels")
-    .WithOpenApi();
+ // Retrieve the set of hotels from the database.
+ app.MapGet("/Hotels", async () => 
+ {
+     var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+     return hotels;
+ })
+     .WithName("GetHotels")
+     .WithOpenApi();
 
-// Retrieve the bookings for a specific hotel.
-app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
-{
-    throw new NotImplementedException();
-})
-    .WithName("GetBookingsForHotel")
-    .WithOpenApi();
+ // Retrieve the bookings for a specific hotel.
+ app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+ {
+     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+     return bookings;
+ })
+     .WithName("GetBookingsForHotel")
+     .WithOpenApi();
 
-// Retrieve the bookings for a specific hotel that are after a specified date.
-app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
-{
-    throw new NotImplementedException();
-})
-    .WithName("GetRecentBookingsForHotel")
-    .WithOpenApi();
+ // Retrieve the bookings for a specific hotel that are after a specified date.
+ app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+ {
+     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+     return bookings;
+ })
+     .WithName("GetRecentBookingsForHotel")
+     .WithOpenApi();
 
 // This endpoint is used to send a message to the Azure OpenAI endpoint.
-app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
-{
-    var message = await Task.FromResult(request.Form["message"]);
-    
-    return "This endpoint is not yet available.";
-})
-    .WithName("Chat")
-    .WithOpenApi();
+ app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
+ {
+     var message = await Task.FromResult(request.Form["message"]);
+     var kernel = app.Services.GetRequiredService<Kernel>();
+     var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+     var executionSettings = new OpenAIPromptExecutionSettings
+     {
+         ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+     };
+     var response = await chatCompletionService.GetChatMessageContentAsync(message.ToString(), executionSettings, kernel);
+     return response?.Content!;
+ })
+     .WithName("Chat")
+     .WithOpenApi();
+
 
 // This endpoint is used to vectorize a text string.
 // We will use this to generate embeddings for the maintenance request text.
@@ -108,7 +141,9 @@ app.MapGet("/Vectorize", async (string text, [FromServices] IVectorizationServic
 app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromServices] IVectorizationService vectorizationService, int max_results = 0, double minimum_similarity_score = 0.8) =>
 {
     // Exercise 3 Task 3 TODO #3: Insert code to call the ExecuteVectorSearch function on the Vectorization Service. Don't forget to remove the NotImplementedException.
-    throw new NotImplementedException();
+    var results = await vectorizationService.ExecuteVectorSearch(queryVector, max_results, minimum_similarity_score);
+    return results;
+
 })
     .WithName("VectorSearch")
     .WithOpenApi();
